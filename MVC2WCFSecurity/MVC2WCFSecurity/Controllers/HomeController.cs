00423b2f93bc46332c9e62c1b43643ef61ed0cff
Zak Whitaker <zak.whitaker@gmail.com>
2015-05-30 11:36:43 -0400

    Initial commit

---
+++ MVC2WCFSecurity/MVC2WCFSecurity/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.ServiceModel.Security;
+using System.Security.Cryptography.X509Certificates;
+using System.Net.Security;
+using System.Net;
+
+namespace MVC2WCFSecurity.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ServiceReference.Service1Client test = new ServiceReference.Service1Client();
+
+            ServicePointManager.ServerCertificateValidationCallback = new RemoteCertificateValidationCallback(OnValidationCallback);
+
+            test.ClientCredentials.UserName.UserName = "test";
+            test.ClientCredentials.UserName.Password = "test";
+
+            ViewBag.Message = test.GetData(10);
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your app description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public static bool OnValidationCallback(object sender, X509Certificate cert, X509Chain chain, SslPolicyErrors errors)
+        {
+            return true;
+        }
+    }
+}
---
+++ WcfService1/UserNamePassValidator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IdentityModel.Selectors;
+using System.ServiceModel;
+
+namespace Service
+{
+    class UserNamePassValidator : UserNamePasswordValidator
+    {
+        public override void Validate(string userName, string password)
+        {
+            if (userName == null || password == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            if (!(userName == "test" && password == "test"))
+            {
+                throw new FaultException("Incorrect Username or Password");
+            }
+        }
+    }
+}
